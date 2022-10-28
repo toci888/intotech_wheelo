fab65f266d6b3664e7710780563b73cd3db42436
Srebrny <Srebrny@DESKTOP-7JT22M3>
2022-10-28 09:48:07 +0200

    Interfaces and Logic Classes

---
+++ Toci.Driver.Bll.Porsche.Interfaces/Intotech.Wheelo.Bll.Persistence/VfriendLogic.cs
@@ -0,0 +1,14 @@
+﻿using Intotech.Wheelo.Bll.Persistence.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Toci.Driver.Database.Persistence.Models;
+
+namespace Intotech.Wheelo.Bll.Persistence
+{
+    public class VfriendLogic : Logic<Vfriend>, IVfriendLogic
+    {
+    }
+}
