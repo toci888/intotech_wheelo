feef8d2f5ed2c9b94f574c367a2ec200d8223d6c
kacper8989 <kacper.wybierala1@gmail.com>
2022-11-08 10:15:50 +0100

    fix connect with expo

--- Client-Mobile/environment.ts
+++ Client-Mobile/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    production: false,
-    apiUrl: 'http://192.168.1.96:5105/'
-}
+  production: false,
+  apiUrl: 'http://192.168.1.96:5105/',
+};
--- Toci.Driver.Bll.Porsche.Interfaces/Toci.Driver.Api/Program.cs
+++ Toci.Driver.Bll.Porsche.Interfaces/Toci.Driver.Api/Program.cs
@@ -10,7 +10,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.WebHost.ConfigureKestrel(options =>
 {
     options.ListenAnyIP(5105); // to listen for incoming http connection on port 5001
-    options.ListenAnyIP(7105, configure => configure.UseHttps()); // to listen for incoming https connection on port 7001
+    //options.ListenAnyIP(7105, configure => configure.UseHttps()); // to listen for incoming https connection on port 7001
 });
 
 // Add services to the container.
