7d14b17fbf5ac8a3119ac1ae590f2eb831ee63bc
Bartomiej Zapart <bzapart@hotmail.com>
2022-11-08 19:54:18 +0100

    fix

--- Toci.Driver.Bll.Porsche.Interfaces/Intotech.Wheelo.Tests/Persistence.Seed/SeedWorktrip.cs
+++ Toci.Driver.Bll.Porsche.Interfaces/Intotech.Wheelo.Tests/Persistence.Seed/SeedWorktrip.cs
@@ -13,9 +13,16 @@ namespace Intotech.Wheelo.Tests.Persistence.Seed
         {
             List<Worktrip> worktripList = new List<Worktrip>();
 
-            Worktrip wt = new Worktrip() { Idaccount = 2, Latitudefrom = 52.23151900096433, Longitudefrom = 21.083137973493866 }; //52.23151900096433, 21.083137973493866
-
+            Worktrip wt = new Worktrip() { Idaccount = 2, Latitudefrom = 52.24602984098752, Longitudefrom = 21.08428642005573,
+                Latitudeto = 52.20678607141056, Longitudeto = 21.0108489021064, Fromhour = new TimeOnly(8, 0), 
+                Tohour = new TimeOnly(16, 0) }; //52.23151900096433, 21.083137973493866
             worktripList.Add(wt);
+
+            worktripList.Add(new Worktrip() { Idaccount = 3, Latitudefrom = 52.24587632734148, Longitudefrom = 21.087258004124745,
+                Latitudeto = 52.20478607141056, Longitudeto = 21.0118489021064, Fromhour = new TimeOnly(7, 50), 
+                Tohour = new TimeOnly(16, 10)});
+
+            
 
             InsertCollection(worktripList);
         }
