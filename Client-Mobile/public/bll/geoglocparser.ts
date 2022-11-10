9cdfb555aee349dacaf4964519807118ef933483
kacper8989 <kacper.wybierala1@gmail.com>
2022-11-10 21:19:18 +0100

    little improvement

--- Client-Mobile/public/bll/geoglocparser.ts
+++ Client-Mobile/public/bll/geoglocparser.ts
@@ -1,40 +1,55 @@
-import { GeogLocModel } from "../models/geoglocmodel";
+import { GeogLocModel } from '../models/geoglocmodel';
 
+export class GeogLocDataParser {
+  constructor() {}
 
-export class GeogLocDataParser
-{
-    constructor()
+  Mapper = [
     {
+      route: (json, model: GeogLocModel) => {
+        model.Street = json.long_name;
 
-    }
-
-    Mapper = [
-        {"route": (json, model: GeogLocModel) => { model.Street = json.long_name; return model; } },
-        {"street_number": (json, model: GeogLocModel) => { model.HouseNumber = json.long_name; return model; } },
-        {"postal_code": (json, model: GeogLocModel) => { model.PostCode = json.long_name; return model; } },
-        {"locality": (json, model: GeogLocModel) => { model.City = json.long_name; return model; } },
-    ];
-
-    model: GeogLocModel = new GeogLocModel();
-
-    ParseGeogLocResult(result: any)
+        return model;
+      },
+    },
     {
-        result.result.address_components.forEach(element => {
+      street_number: (json, model: GeogLocModel) => {
+        model.HouseNumber = json.long_name;
 
-            this.Mapper.forEach(mapEl => {
+        return model;
+      },
+    },
+    {
+      postal_code: (json, model: GeogLocModel) => {
+        model.PostCode = json.long_name;
 
-                if (mapEl[element.types[0]] !== undefined)
-                {
-                    this.model = mapEl[element.types[0]](element, this.model);
-                }
-            });
-        });
+        return model;
+      },
+    },
+    {
+      locality: (json, model: GeogLocModel) => {
+        model.City = json.long_name;
+        
+        return model;
+      },
+    },
+  ];
 
-console.log("geometry", result.result.geometry);
+  model: GeogLocModel = new GeogLocModel();
 
-        this.model.Latitude = result.result.geometry.location.lat;
-        this.model.Longitude = result.result.geometry.location.lng;
+  ParseGeogLocResult(result: any) {
+    result.result.address_components.forEach((element) => {
+      this.Mapper.forEach((mapEl) => {
+        if (mapEl[element.types[0]] !== undefined) {
+          this.model = mapEl[element.types[0]](element, this.model);
+        }
+      });
+    });
 
-        return this.model;
-    }
+    console.log("geometry", result.result.geometry);
+
+    this.model.Latitude = result.result.geometry.location.lat;
+    this.model.Longitude = result.result.geometry.location.lng;
+
+    return this.model;
+  }
 }
--- Client-Mobile/public/models/geoglocmodel.ts
+++ Client-Mobile/public/models/geoglocmodel.ts
@@ -1,17 +1,10 @@
-import { Double } from "react-native/Libraries/Types/CodegenTypes";
+import { Double } from 'react-native/Libraries/Types/CodegenTypes';
 
-export class GeogLocModel
-{
-    constructor()
-    {
-
-    }
-    
-    Latitude: Double;
-    Longitude: Double;
-    PostCode: string;
-    City: string;
-    Street: string;
-    HouseNumber: number;
+export class GeogLocModel {
+  Latitude!: Double;
+  Longitude!: Double;
+  PostCode!: string;
+  City!: string;
+  Street!: string;
+  HouseNumber!: number;
 }
-
