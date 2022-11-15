3660f821282444014e0ab654d1349f79963a8e73
Bartomiej Zapart <bzapart@hotmail.com>
2022-11-15 12:02:36 +0100

    task for Robert - cars txt.

---
+++ Toci.Driver.Bll.Porsche.Interfaces/Intotech.Wheelo.Tests/Persistence.Seed/CarsTxtParser.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intotech.Wheelo.Tests.Persistence.Seed
+{
+    public class CarsTxtParser
+    {
+        public virtual List<string> GetCarsBrands(string path)
+        {
+            List<string> carsBrands = new List<string>();
+
+            // wczytac plik do zmiennej string
+            string content = File.ReadAllText(path);
+            // 
+
+            string[] elementsToFurtherSplit = content.Split('\n');
+
+            foreach (string element in elementsToFurtherSplit)
+            {
+                element.Split('-');
+                //content.
+            }
+
+            return carsBrands;
+        }
+    }
+}
---
+++ Toci.Driver.Bll.Porsche.Interfaces/Intotech.Wheelo.Tests/Persistence.Seed/SeedAutomaticCars.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Toci.Driver.Database.Persistence.Models;
+
+namespace Intotech.Wheelo.Tests.Persistence.Seed
+{
+    internal class SeedAutomaticCars : SeedLogic<Carsbrand>
+    {
+        public override void Insert()
+        {
+            List<Carsbrand> carsBr = new List<Carsbrand>();
+
+            CarsTxtParser carsTxtParser = new CarsTxtParser();
+
+            List<string> cars = carsTxtParser.GetCarsBrands("");
+
+            foreach (string item in cars)
+            {
+                carsBr.Add(new Carsbrand() { Brand = item });
+            }
+
+            InsertCollection(carsBr);
+        }
+    }
+}
