b1bd7aa60ca308a36a70e35e5a439d48f6cff15d
Bartomiej Zapart <bzapart@hotmail.com>
2022-11-16 13:58:40 +0100

    colour, cars brands logic.

---
+++ Toci.Driver.Bll.Porsche.Interfaces/Intotech.Wheelo.Bll.Bentley.Interfaces/IColourManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Toci.Driver.Database.Persistence.Models;
+
+namespace Intotech.Wheelo.Bll.Bentley.Interfaces
+{
+    public interface IColourManager
+    {
+        // 
+        List<Colour> GetColoursForWildcard(string beginning);
+    }
+}
---
+++ Toci.Driver.Bll.Porsche.Interfaces/Intotech.Wheelo.Bll.Bentley/ColourManager.cs
@@ -0,0 +1,22 @@
+﻿using Intotech.Wheelo.Bll.Bentley.Interfaces;
+using Toci.Driver.Database.Persistence.Models;
+
+namespace Intotech.Wheelo.Bll.Bentley
+{
+    public class ColourManager : IColourManager
+    {
+
+       //protected IColourLogic ColoursLogic;
+
+        /*public ColourManager(IColourLogic coloursLogic)
+        {
+            ColoursLogic = coloursLogic;
+        }*/
+
+        public virtual List<Colour> GetColoursForWildcard(string beginning)
+        {
+           // return ColoursLogic.Select(m => m.Name.StartsWith(beginning)).ToList();
+            return null;
+        }
+    }
+}
