86f61ff0372f26ea11eff906b7a49932ebeea5a2
Bartomiej Zapart <bzapart@hotmail.com>
2022-11-16 10:36:06 +0100

    seeding fix.

--- Toci.Driver.Bll.Porsche.Interfaces/Intotech.Wheelo.Tests/Persistence.Seed/SeedCars.cs
+++ Toci.Driver.Bll.Porsche.Interfaces/Intotech.Wheelo.Tests/Persistence.Seed/SeedCars.cs
@@ -13,8 +13,8 @@ namespace Intotech.Wheelo.Tests.Persistence.Seed
         {
             List<Car> cars = new List<Car>();
 
-            cars.Add(new Car() { Idaccounts = 1, Availableseats = 3, Idcarsbrands = 1, Idcarsmodels = 1, Registrationplate = "OPO 80F4" });
-            cars.Add(new Car() { Idaccounts = 2, Availableseats = 4, Idcarsbrands = 3, Idcarsmodels = 7, Registrationplate = "OP 1667L" });
+            cars.Add(new Car() { Idaccounts = 1, Availableseats = 3, Idcolours = 3, Idcarsbrands = 1, Idcarsmodels = 1, Registrationplate = "OPO 80F4" });
+            cars.Add(new Car() { Idaccounts = 2, Availableseats = 4, Idcolours = 8, Idcarsbrands = 3, Idcarsmodels = 7, Registrationplate = "OP 1667L" });
 
             InsertCollection(cars);
         }
