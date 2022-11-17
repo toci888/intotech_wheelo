f3fc2531ab073dd0e5fa4d971f6fad5c3f90312e
Srebrny <Srebrny@DESKTOP>
2022-11-17 10:54:26 +0100

    CarsBrandsModelsController

---
+++ Toci.Driver.Bll.Porsche.Interfaces/Intotech.Wheelo.Cmc.Api/Controllers/CarsBrandsModelsController.cs
@@ -0,0 +1,23 @@
+﻿using Intotech.Common.Microservices;
+using Intotech.Wheelo.Bll.Bentley.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Toci.Driver.Database.Persistence.Models;
+
+namespace Intotech.Wheelo.Cmc.Api.Controllers
+{
+	[ApiController]
+	[Route("api/[controller]")]
+	public class CarsBrandsModelsController : ApiSimpleControllerBase<ICarsBrandsModelManager>
+	{
+		public CarsBrandsModelsController(ICarsBrandsModelManager logic) : base(logic)
+		{
+		}
+
+		[HttpGet]
+		[Route("get-cars-brands-for-wildcard")]
+		public List<Carsbrand> GetCarsBrandsForWildcard(string beginning)
+		{
+			return Logic.GetCarsBrandsForWildcard(beginning);
+		}
+	}
+}
