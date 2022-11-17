ab8377d584a95103a08e3f6b548495febf5b7f4f
Bartomiej Zapart <bzapart@hotmail.com>
2022-11-17 10:11:42 +0100

    cmc api microservice, colours wildcards.

---
+++ Toci.Driver.Bll.Porsche.Interfaces/Intotech.Wheelo.Cmc.Api/Controllers/ColoursController.cs
@@ -0,0 +1,23 @@
+﻿using Intotech.Common.Microservices;
+using Intotech.Wheelo.Bll.Bentley.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Toci.Driver.Database.Persistence.Models;
+
+namespace Intotech.Wheelo.Cmc.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ColoursController : ApiSimpleControllerBase<IColourManager>
+    {
+        public ColoursController(IColourManager logic) : base(logic)
+        {
+        }
+
+        [HttpGet]
+        [Route("get-colours-for-wildcard")]
+        public List<Colour> GetColoursForWildcard(string beginning)
+        {
+            return Logic.GetColoursForWildcard(beginning);
+        }
+    }
+}
---
+++ Toci.Driver.Bll.Porsche.Interfaces/Intotech.Wheelo.Cmc.Api/Program.cs
@@ -0,0 +1,34 @@
+using Intotech.Wheelo.Bll.Bentley;
+using Intotech.Wheelo.Bll.Bentley.Interfaces;
+using Intotech.Wheelo.Bll.Persistence;
+using Intotech.Wheelo.Bll.Persistence.Interfaces;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IColourLogic, ColourLogic>();
+
+builder.Services.AddScoped<IColourManager, ColourManager>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
