680992c098ea8f2069d401d951ad1bba9a4abb69
Robert Kulig <new.dev1975@gmail.com>
2022-11-18 03:46:03 +0100

    Added Interfaces Logic to Intotech.Wheelo.Social.Bll.Persistence.Interfaces

---
+++ Toci.Driver.Bll.Porsche.Interfaces/Intotech.Wheelo.Social.Bll.Persistence.Interfaces/IOrganuzeMeetingLogic.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Intotech.Common.Bll.Interfaces;
+using Intotech.Wheelo.Social.Database.Persistence.Models;
+
+namespace Intotech.Wheelo.Social.Bll.Persistence.Interfaces
+{
+    public interface IOrganizeMeetingLogic : ILogicBase<Organizemeeting>
+    {
+        
+    }
+}
