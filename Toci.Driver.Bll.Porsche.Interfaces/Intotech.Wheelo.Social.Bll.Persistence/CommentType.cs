c41cf15045fbbb1d8bfa27c23c969bc73cddf0f1
Srebrny <Srebrny@DESKTOP>
2022-11-18 10:47:17 +0100

    bll persistens logic

---
+++ Toci.Driver.Bll.Porsche.Interfaces/Intotech.Wheelo.Social.Bll.Persistence/CommentType.cs
@@ -0,0 +1,13 @@
+﻿using Intotech.Wheelo.Social.Bll.Persistence.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intotech.Wheelo.Social.Bll.Persistence
+{
+    public class CommentType : Logic<CommentType>, IcommentType
+    {
+    }
+}
