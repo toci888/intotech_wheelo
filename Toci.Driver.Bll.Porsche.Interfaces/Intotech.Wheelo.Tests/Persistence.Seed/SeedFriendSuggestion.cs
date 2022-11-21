4e9d3d8f447d902ea2a7a7256af26e25ff3bc533
Julia <Julia@Intotech>
2022-11-21 14:42:24 +0000

    SeedInvitation

--- Toci.Driver.Bll.Porsche.Interfaces/Intotech.Wheelo.Tests/Persistence.Seed/SeedFriendSuggestion.cs
+++ Toci.Driver.Bll.Porsche.Interfaces/Intotech.Wheelo.Tests/Persistence.Seed/SeedFriendSuggestion.cs
@@ -24,16 +24,16 @@ namespace Intotech.Wheelo.Tests.Persistence.Seed
             list.Add(new Friendsuggestion() { Idaccount = 9, Idsuggested = 35, Idsuggestedfriend = 36 });
            
             list.Add(new Friendsuggestion() { Idaccount = 10, Idsuggested = 14, Idsuggestedfriend = 25 });
-            list.Add(new Friendsuggestion() { Idaccount = 11, Idsuggested = 15, Idsuggestedfriend = 23 });
+            list.Add(new Friendsuggestion() { Idaccount = 11, Idsuggested = 15, Idsuggestedfriend = 12 });
             list.Add(new Friendsuggestion() { Idaccount = 12, Idsuggested = 14, Idsuggestedfriend = 23 });
-            list.Add(new Friendsuggestion() { Idaccount = 13, Idsuggested = 14, Idsuggestedfriend = 23 });
-            list.Add(new Friendsuggestion() { Idaccount = 14, Idsuggested = 14, Idsuggestedfriend = 23 });
-            list.Add(new Friendsuggestion() { Idaccount = 15, Idsuggested = 14, Idsuggestedfriend = 23 });
-            list.Add(new Friendsuggestion() { Idaccount = 16, Idsuggested = 14, Idsuggestedfriend = 23 });
-            list.Add(new Friendsuggestion() { Idaccount = 17, Idsuggested = 34, Idsuggestedfriend = 23 });
-            list.Add(new Friendsuggestion() { Idaccount = 18, Idsuggested = 17, Idsuggestedfriend = 23 });
-            list.Add(new Friendsuggestion() { Idaccount = 19, Idsuggested = 4, Idsuggestedfriend = 23 });
-            list.Add(new Friendsuggestion() { Idaccount = 20, Idsuggested = 5, Idsuggestedfriend = 43 });
+            list.Add(new Friendsuggestion() { Idaccount = 13, Idsuggested = 14, Idsuggestedfriend = 27 });
+            list.Add(new Friendsuggestion() { Idaccount = 14, Idsuggested = 44, Idsuggestedfriend = 24 });
+            list.Add(new Friendsuggestion() { Idaccount = 15, Idsuggested = 17, Idsuggestedfriend = 18 });
+            list.Add(new Friendsuggestion() { Idaccount = 16, Idsuggested = 4, Idsuggestedfriend = 17 });
+            list.Add(new Friendsuggestion() { Idaccount = 17, Idsuggested = 34, Idsuggestedfriend = 15 });
+            list.Add(new Friendsuggestion() { Idaccount = 18, Idsuggested = 17, Idsuggestedfriend = 47 });
+            list.Add(new Friendsuggestion() { Idaccount = 19, Idsuggested = 4, Idsuggestedfriend = 22 });
+            list.Add(new Friendsuggestion() { Idaccount = 20, Idsuggested = 5, Idsuggestedfriend = 11 });
 
 
             InsertCollection(list);
---
+++ Toci.Driver.Bll.Porsche.Interfaces/Intotech.Wheelo.Tests/Persistence.Seed/SeedInvitation.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intotech.Wheelo.Tests.Persistence.Seed
+{
+    internal class SeedInvitation
+    {
+    }
+}
