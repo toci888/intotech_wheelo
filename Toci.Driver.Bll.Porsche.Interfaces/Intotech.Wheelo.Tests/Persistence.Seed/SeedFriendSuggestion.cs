0a766c565802dd5f33f10f66b72ab4731f0e157a
Srebrny <Srebrny@DESKTOP>
2022-11-21 10:30:02 +0100

    seed new friendsuggestion by krzys

--- Toci.Driver.Bll.Porsche.Interfaces/Intotech.Wheelo.Tests/Persistence.Seed/SeedFriendSuggestion.cs
+++ Toci.Driver.Bll.Porsche.Interfaces/Intotech.Wheelo.Tests/Persistence.Seed/SeedFriendSuggestion.cs
@@ -15,6 +15,13 @@ namespace Intotech.Wheelo.Tests.Persistence.Seed
 
             list.Add(new Friendsuggestion() { Idaccount = 1, Idsuggested = 14, Idsuggestedfriend = 23 });
             list.Add(new Friendsuggestion() { Idaccount = 2, Idsuggested = 13, Idsuggestedfriend = 22 });
+            list.Add(new Friendsuggestion() { Idaccount = 3, Idsuggested = 11, Idsuggestedfriend = 12 });
+            list.Add(new Friendsuggestion() { Idaccount = 4, Idsuggested = 22, Idsuggestedfriend = 23 });
+            list.Add(new Friendsuggestion() { Idaccount = 5, Idsuggested = 33, Idsuggestedfriend = 34 });
+            list.Add(new Friendsuggestion() { Idaccount = 6, Idsuggested = 44, Idsuggestedfriend = 45 });
+            list.Add(new Friendsuggestion() { Idaccount = 7, Idsuggested = 15, Idsuggestedfriend = 16 });
+            list.Add(new Friendsuggestion() { Idaccount = 8, Idsuggested = 25, Idsuggestedfriend = 26 });
+            list.Add(new Friendsuggestion() { Idaccount = 9, Idsuggested = 35, Idsuggestedfriend = 36 });
 
 
             InsertCollection(list);
