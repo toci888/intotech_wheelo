e55ad9c101be979651d559137dcadba2ad57e73c
Bartomiej Zapart <bzapart@hotmail.com>
2022-11-22 17:27:01 +0100

    date time only dtos.

---
+++ Toci.Driver.Bll.Porsche.Interfaces/Intotech.Wheelo.Bll.Models/TimeModels/DateOnlyDto.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intotech.Wheelo.Bll.Models.TimeModels
+{
+    public class DateOnlyDto
+    {
+        public int Year { get; set; }
+        public int Month { get; set; }
+        public int Day { get; set; }
+
+        public DateOnlyDto()
+        {
+
+        }
+
+        public DateOnlyDto(DateOnly date)
+        {
+            Year = date.Year;
+            Month = date.Month;
+            Day = date.Day;
+        }
+
+        public DateOnlyDto(int year, int month, int day)
+        {
+            Year = year;
+            Month = month;
+            Day = day;
+        }
+
+        public DateOnly GetDate()
+        {
+            return new DateOnly(Year, Month, Day);
+        }
+    }
+}
---
+++ Toci.Driver.Bll.Porsche.Interfaces/Intotech.Wheelo.Bll.Models/TimeModels/TimeOnlyDto.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intotech.Wheelo.Bll.Models.TimeModels
+{
+    public class TimeOnlyDto
+    {
+        public int Hour { get; set; }
+        public int Minute { get; set; }
+
+        public TimeOnlyDto()
+        {
+            
+        }
+
+        public TimeOnlyDto(int hour, int minute)
+        {
+            Hour = hour;
+            Minute = minute;
+        }
+
+        public TimeOnlyDto(TimeOnly time)
+        {
+            Hour = time.Hour;
+            Minute = time.Minute;
+        }
+
+        public TimeOnly GetTime()
+        {
+            return new TimeOnly(Hour, Minute);
+        }
+    }
+}
