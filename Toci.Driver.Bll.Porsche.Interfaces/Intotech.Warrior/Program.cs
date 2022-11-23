b16009d3b260b7787a73e789eb43f34268c5ac36
Srebrny <Srebrny@DESKTOP>
2022-11-23 10:59:15 +0100

    krzys

--- Toci.Driver.Bll.Porsche.Interfaces/Intotech.Warrior/Program.cs
+++ Toci.Driver.Bll.Porsche.Interfaces/Intotech.Warrior/Program.cs
@@ -17,11 +17,11 @@ Console.WriteLine("Warrior is seeding your dbs....");
 //sa.Insert();
 //new SeedFriendSuggestion().Insert();
 
-//new WheeloMainSeedManager().SeedAllDb();
+new WheeloMainSeedManager().SeedAllDb();
 
-//new SocialSeedManager().SeedAllDb();
-new SeedTrip().Insert();
-new SeedTripParticipants().Insert();
+new SocialSeedManager().SeedAllDb();
+//new SeedTrip().Insert();
+//new SeedTripParticipants().Insert();
 //TypeScriptModelsGenerator typeScriptModelsGenerator = new TypeScriptModelsGenerator();
 
 //typeScriptModelsGenerator.Generate(Assembly.GetAssembly(typeof(Tripparticipant)));
