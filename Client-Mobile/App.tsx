be0204eb78f336253da05a79f74a057244459175
Kacper Wybierala <1111111191kapi@gmail.com>
2022-11-23 14:47:00 +0100

    chat js init

--- Client-Mobile/App.tsx
+++ Client-Mobile/App.tsx
@@ -1,30 +1,28 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { Text } from 'react-native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
-
-const Stack = createStackNavigator();
-
-const HomeScreen = () => <Text>Home Screen</Text>;
-
-const NavigationStack = () => {
-  return (
-    <Stack.Navigator>
-      <Stack.Screen name='Home' component={HomeScreen} />
-    </Stack.Navigator>
-  );
-};
+import { NavigationStack } from './src/Screens/NavigationStack';
+import { AppProvider } from './src/Hooks/AppContextHook';
 
 export default () => {
   return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
-    <SafeAreaView style={{ flex: 1 }}>
-      <NavigationContainer>
-        <NavigationStack />
-      </NavigationContainer>
-    </SafeAreaView>
-    </GestureHandlerRootView>
+    <AppProvider>
+      <GestureHandlerRootView style={{ flex: 1 }}>
+        <SafeAreaView style={{ flex: 1 }}>
+          <NavigationContainer>
+            <NavigationStack />
+          </NavigationContainer>
+        </SafeAreaView>
+      </GestureHandlerRootView>
+    </AppProvider>
   );
 };
+
+// const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
