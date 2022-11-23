9f05da840e554a5f4099f4b41660cc992f13297c
Julia <Julia@Intotech>
2022-11-23 14:30:24 +0000

    Ucommited changes

--- Toci.Driver.Bll.Porsche.Interfaces/Intotech.Wheelo.Tests/Persistence.Seed/SeedTrip.cs
+++ Toci.Driver.Bll.Porsche.Interfaces/Intotech.Wheelo.Tests/Persistence.Seed/SeedTrip.cs
@@ -13,26 +13,27 @@ namespace Intotech.Wheelo.Tests.Persistence.Seed
         {
             List<Trip> tripList = new List<Trip>();
 
+            //1
             tripList.Add(new Trip()
             {
                 Fromhour = new TimeOnly(8, 0),
                 Tohour = new TimeOnly(16, 0),
                 Idinitiatoraccount = 1,
                 Iscurrent = true,
-                Leftseats = 1,
+                Leftseats = 2,
                 Tripdate = new DateOnly(2022, 11, DateTime.Now.Day + 1)
             }) ;
-
+            //2
             tripList.Add(new Trip()
             {
                 Fromhour = new TimeOnly(7, 0),
                 Tohour = new TimeOnly(10, 0),
                 Idinitiatoraccount = 2,
                 Iscurrent = true,
-                Leftseats = 3,
+                Leftseats = 2,
                 Tripdate = new DateOnly(2022, 11, DateTime.Now.Day + 1)
             });
-
+            //3
             tripList.Add(new Trip()
             {
                 Fromhour = new TimeOnly(17, 0),
@@ -42,7 +43,7 @@ namespace Intotech.Wheelo.Tests.Persistence.Seed
                 Leftseats = 1,
                 Tripdate = new DateOnly(2022, 11, DateTime.Now.Day + 1)
             });
-
+            //4
             tripList.Add(new Trip()
             {
                 Fromhour = new TimeOnly(14, 0),
@@ -52,36 +53,57 @@ namespace Intotech.Wheelo.Tests.Persistence.Seed
                 Leftseats = 1,
                 Tripdate = new DateOnly(2022, 11, DateTime.Now.Day + 1)
             });
-
+            //5
             tripList.Add(new Trip()
             {
                 Fromhour = new TimeOnly(11, 0),
                 Tohour = new TimeOnly(12, 0),
                 Idinitiatoraccount = 2,
                 Iscurrent = true,
-                Leftseats = 1,
+                Leftseats = 2,
                 Tripdate = new DateOnly(2022, 11, DateTime.Now.Day + 1)
             });
-
+            //6
             tripList.Add(new Trip()
             {
                 Fromhour = new TimeOnly(21, 0),
                 Tohour = new TimeOnly(22, 0),
                 Idinitiatoraccount = 3,
                 Iscurrent = true,
-                Leftseats = 1,
+                Leftseats = 2,
                 Tripdate = new DateOnly(2022, 11, DateTime.Now.Day + 1)
             });
-
+            //7
             tripList.Add(new Trip()
             {
-                Fromhour = new TimeOnly(13, 0),
-                Tohour = new TimeOnly(14, 0),
+                Fromhour = new TimeOnly(16, 0),
+                Tohour = new TimeOnly(17, 0),
                 Idinitiatoraccount = 2,
                 Iscurrent = true,
                 Leftseats = 1,
                 Tripdate = new DateOnly(2022, 11, DateTime.Now.Day + 1)
             });
+            //8
+            tripList.Add(new Trip()
+            {
+                Fromhour = new TimeOnly(17, 0),
+                Tohour = new TimeOnly(21, 0),
+                Idinitiatoraccount = 2,
+                Iscurrent = true,
+                Leftseats = 1,
+                Tripdate = new DateOnly(2022, 11, DateTime.Now.Day + 1)
+            });
+            //9
+            tripList.Add(new Trip()
+            {
+                Fromhour = new TimeOnly(18, 0),
+                Tohour = new TimeOnly(20, 0),
+                Idinitiatoraccount = 2,
+                Iscurrent = true,
+                Leftseats = 3,
+                Tripdate = new DateOnly(2022, 11, DateTime.Now.Day + 1)
+            });
+           
             // ?
 
             InsertCollection(tripList);
