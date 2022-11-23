63e02db81dc9448d85c58895ce0b8b280a19ff42
Bartomiej Zapart <bzapart@hotmail.com>
2022-11-23 17:01:33 +0100

    occupations progress.

--- Toci.Driver.Bll.Porsche.Interfaces/Intotech.Wheelo.Tests/Persistence.Seed/ProfessionsTxtParser.cs
+++ Toci.Driver.Bll.Porsche.Interfaces/Intotech.Wheelo.Tests/Persistence.Seed/ProfessionsTxtParser.cs
@@ -9,9 +9,23 @@ namespace Intotech.Wheelo.Tests.Persistence.Seed
 {
     public class ProfessionsTxtParser : SeedLogic<Occupation>
     {
+        private const string HerebyDoc = "Niniejszy dokument";
+
         public override void Insert()
         {
-            
+            string file = File.ReadAllText("../../../../../SQL/professions.txt");
+
+            string[] occupationLines = file.Split("\n");
+
+            foreach (string profession in occupationLines)
+            {
+                if (!profession.Contains(HerebyDoc))
+                {
+                    string[] elements = profession.Split(" ");
+                }
+            }
+
+            InsertCollection(ModelsEntities);
         }
     }
 }
