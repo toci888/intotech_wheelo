d18b4abb19a2963ae5e65d9ae742b34429562438
Bartomiej Zapart <bzapart@hotmail.com>
2022-12-04 20:29:19 +0100

    some basic chat api attempts.

---
+++ Intotech.Wheelo.Chat.Jaguar/MessagesService.cs
@@ -0,0 +1,28 @@
+﻿using Intotech.Wheelo.Chat.Bll.Persistence.Interfaces;
+using Intotech.Wheelo.Chat.Database.Persistence.Models;
+using Intotech.Wheelo.Chat.Jaguar.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intotech.Wheelo.Chat.Jaguar
+{
+    public class MessagesService : IMessagesService
+    {
+        protected IMessageLogic MessageLogic;
+
+        public MessagesService(IMessageLogic messageLogic)
+        {
+            MessageLogic = messageLogic;
+        }
+
+        public virtual Message AddMessage(Message message)
+        {
+            Message result = MessageLogic.Insert(message);
+
+            return result;
+        }
+    }
+}
