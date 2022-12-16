c07fb1fb219e017c215081260f67fd7fa33b436f
Julia <Julia>
2022-12-16 13:18:27 +0000

    SmOccCrat

---
+++ Toci.Driver.Bll.Porsche.Interfaces/Intotech.Wheelo.Bll.Porsche/User/UserMetaService.cs
@@ -0,0 +1,34 @@
+﻿using Intotech.Common;
+using Intotech.Common.Bll.ComplexResponses;
+using Intotech.Wheelo.Bll.Models.Account;
+using Intotech.Wheelo.Bll.Persistence.Interfaces;
+using Intotech.Wheelo.Bll.Porsche.Interfaces.User;
+using Npgsql.Internal.TypeMapping;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Toci.Driver.Database.Persistence.Models;
+
+namespace Intotech.Wheelo.Bll.Porsche.User
+{
+    public class UserMetaService : IUserMetaService
+    {
+        protected IOccupationSmokerCratLogic OccSmoCrLogic;
+
+        public UserMetaService(IOccupationSmokerCratLogic occSmoCrLogic)
+        {
+            OccSmoCrLogic = occSmoCrLogic;
+        }
+
+        public ReturnedResponse<SmokerOccupationDto> SetSmokerOccupation(SmokerOccupationDto smokerOccupationDto)
+        {
+            Occupationsmokercrat model = DtoModelMapper.Map<Occupationsmokercrat, SmokerOccupationDto>(smokerOccupationDto);
+
+            OccSmoCrLogic.Insert(model);
+
+            return null;
+        }
+    }
+}
