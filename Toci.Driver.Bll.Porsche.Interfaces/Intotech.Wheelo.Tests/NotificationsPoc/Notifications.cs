c095cbbd8999ec65221de2aa8829075a6c1bf988
Bartomiej Zapart <bzapart@hotmail.com>
2022-12-17 10:57:00 +0100

    notifications primary approach.

---
+++ Toci.Driver.Bll.Porsche.Interfaces/Intotech.Wheelo.Tests/NotificationsPoc/Notifications.cs
@@ -0,0 +1,25 @@
+﻿using ExpoCommunityNotificationServer.Client;
+using ExpoCommunityNotificationServer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intotech.Wheelo.Tests.NotificationsPoc
+{
+    [TestClass]
+    public class Notifications
+    {
+        [TestMethod]
+        public void doopa()
+        {
+            
+            PushApiClient pac = new PushApiClient("ExponentPushToken[XqgL8PLm-p-XsCtlZ_dapr]", new HttpClient() { BaseAddress = new Uri("https://exp.host/") } );
+
+            pac.SetToken("ExponentPushToken[XqgL8PLm-p-XsCtlZ_dapr]");
+
+            PushTicketResponse resp = pac.SendPushAsync(new PushTicketRequest() { PushBody = "Witam Szefa :)", PushSubTitle = "Bartek z tej strony :P" } ).Result;
+        }
+    }
+}
