025833b31b2bf49a0cfe48b6ff345dc37a05c623
Kacper Wybierala <kacper.wybierala1@gmail.com>
2023-01-02 15:52:21 +0100

    api new ip

--- ClientMobile/constants/constants.ts
+++ ClientMobile/constants/constants.ts
@@ -25,7 +25,7 @@ androidHeight += androidNotch;
 
 export const HEADERHEIGHT = Platform.OS === "ios" ? iosHeight : androidHeight;
 
-export const server = "http://20.68.18.151";
+export const server = "http://20.71.35.167";
 
 const serverUrl = server + ":5105/api";
 const integrationApiUrl = server + ":5108/api";
