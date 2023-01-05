39c635b108c795f4fc01220c62ec16fc7f0eaaee
Bartomiej Zapart <bzapart@hotmail.com>
2023-01-05 14:18:52 +0100

    dapper approach

---
+++ Toci.Driver.Bll.Porsche.Interfaces/Intotech.Wheelo.Tests/DapperTests/DapperPoc.cs
@@ -0,0 +1,24 @@
+﻿using System.Data;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Dapper;
+using System.Threading.Tasks;
+
+namespace Intotech.Wheelo.Tests.DapperTests
+{
+    [TestClass]
+    public class DapperPoc
+    {
+        [TestMethod]
+        public void Test()
+        {
+            
+        }
+
+        //protected virtual SqlConnection GetPostgreConnection()
+        //{
+            
+        //}
+    }
+}
