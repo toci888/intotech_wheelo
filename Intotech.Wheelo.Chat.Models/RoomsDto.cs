d7b6c422384ddfb8524112e5cc6f081d26d78de8
Bartomiej Zapart <bzapart@hotmail.com>
2023-01-07 08:52:40 +0100

    fixes in structure, to be refined and tested.

--- Intotech.Wheelo.Chat.Models/RoomsDto.cs
+++ Intotech.Wheelo.Chat.Models/RoomsDto.cs
@@ -9,6 +9,7 @@ namespace Intotech.Wheelo.Chat.Models
     public class RoomsDto
     {
         public int OwnerId { get; set; }
+        public int IdRoom { get; set; }
         public string RoomId { get; set; }
         public string RoomName { get; set; }
         public List<RoomMembersDto> RoomMembers { get; set; }
