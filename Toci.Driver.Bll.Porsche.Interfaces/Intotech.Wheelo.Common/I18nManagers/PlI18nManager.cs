eac8a0dae70980dbe1630692d849f3f8c684bd60
Julia Głocka <glockajulia@gmail.com>
2023-01-15 11:45:32 +0100

    Translations2

--- Toci.Driver.Bll.Porsche.Interfaces/Intotech.Wheelo.Common/I18nManagers/PlI18nManager.cs
+++ Toci.Driver.Bll.Porsche.Interfaces/Intotech.Wheelo.Common/I18nManagers/PlI18nManager.cs
@@ -29,7 +29,24 @@ namespace Intotech.Wheelo.Common.I18nManagers
                 { GetKey(Language, I18nTags.FailedToAddInformation), new I18nModel() { Language = Language, Tag = I18nTags.FailedToAddInformation, Content = "Nie udało się dodać informacji."} },
                 { GetKey(Language, I18nTags.Swedish), new I18nModel() { Language = Language, Tag = I18nTags.Swedish, Content = "Szwedzki"} },
                 { GetKey(Language, I18nTags.Spanish), new I18nModel() { Language = Language, Tag = I18nTags.Spanish, Content = "Hiszpański"} },
+                { GetKey(Language, I18nTags.DataAlreadyExistInDatabase), new I18nModel() { Language = Language, Tag = I18nTags.DataAlreadyExistInDatabase, Content = "Dane już istnieją w bazie danych."} },
+                { GetKey(Language, I18nTags.DefaultModeCreated), new I18nModel() { Language = Language, Tag = I18nTags.DefaultModeCreated, Content = "Domyślny tryb stworzony."} },
+                { GetKey(Language, I18nTags.ErrorPleaseLogInToApp), new I18nModel() { Language = Language, Tag = I18nTags.ErrorPleaseLogInToApp, Content = "Błąd. Zaloguj się proszę do aplikacji."} },
+                { GetKey(Language, I18nTags.EmailDoesNotExist), new I18nModel() { Language = Language, Tag = I18nTags.EmailDoesNotExist, Content = "Adres e-mail nie istnieje."} },
+                { GetKey(Language, I18nTags.FriendshipNotFound), new I18nModel() { Language = Language, Tag = I18nTags.FriendshipNotFound, Content = "Znajomość nie odnaleziona."} },
+               /* { GetKey(Language, I18nTags.LanguageCodeEn), new I18nModel() { Language = Language, Tag = I18nTags.LanguageCodeEn, Content = "Kod języka: angielski."} },
+                { GetKey(Language, I18nTags.LanguageCodePl), new I18nModel() { Language = Language, Tag = I18nTags.LanguageCodePl, Content = "Kod języka: polski."} }, */
+                { GetKey(Language, I18nTags.NoData), new I18nModel() { Language = Language, Tag = I18nTags.NoData, Content = "Brak danych."} },
+                { GetKey(Language, I18nTags.NoWorkTripData), new I18nModel() { Language = Language, Tag = I18nTags.NoWorkTripData, Content = "Brak danych podróży."} },
+                { GetKey(Language, I18nTags.PasswordChangeSuccess), new I18nModel() { Language = Language, Tag = I18nTags.PasswordChangeSuccess, Content = "Udało się zmienić hasło."} },
+                { GetKey(Language, I18nTags.PleaseConfirmYourWheeloAccountRegistration), new I18nModel() { Language = Language, Tag = I18nTags.PleaseConfirmYourWheeloAccountRegistration, Content = "Potwierdź proszę rejestrację swojego konta Wheelo."} },
+                { GetKey(Language, I18nTags.PleaseLogIn), new I18nModel() { Language = Language, Tag = I18nTags.PleaseLogIn, Content = "Proszę, zaloguj się."} },
+                { GetKey(Language, I18nTags.RefreshTokenExpiredPleaseLogIn), new I18nModel() { Language = Language, Tag = I18nTags.RefreshTokenExpiredPleaseLogIn, Content = "Bieżący token wygasł. Proszę, zaloguj się."} },
+                { GetKey(Language, I18nTags.UnderAttack), new I18nModel() { Language = Language, Tag = I18nTags.UnderAttack, Content = "Jesteś atakowany"} },
+                { GetKey(Language, I18nTags.WrongData), new I18nModel() { Language = Language, Tag = I18nTags.WrongData, Content = "Błędne dane."} },
+                { GetKey(Language, I18nTags.WrongOperations), new I18nModel() { Language = Language, Tag = I18nTags.WrongOperations, Content = "Błędne działanie."} },
                 
+                //I18nTags 
 
             };
         }
