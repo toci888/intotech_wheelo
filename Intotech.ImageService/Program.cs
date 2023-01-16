eede592c4d68e833a60a642ebf6375af446b003c
prod <prod@prod>
2023-01-16 13:41:22 +0000

    launch settings of image service.

--- Intotech.ImageService/Program.cs
+++ Intotech.ImageService/Program.cs
@@ -1,5 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
 
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.ListenAnyIP(5072); // to listen for incoming http connection on port 5001
+    //http://20.100.196.118:5105
+    //IPAddress.Parse(20.100.196.118"),
+    //options.ListenAnyIP(7105, configure => configure.UseHttps()); // to listen for incoming https connection on port 7001
+});
+
+
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddControllers();
@@ -14,7 +23,7 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
