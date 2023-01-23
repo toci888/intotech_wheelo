6c0582adb2c48babbb52bc00c7a52fc03a371ca0
Kacper Wybierala <kacper.wybierala1@gmail.com>
2023-01-23 10:43:01 +0100

    delete client-mobile and gatekeeper

--- Client-Mobile/src/Constants/constants.ts
+++
@@ -1,3 +0,0 @@
-export const GOOGLE_MAPS_API_KEY = 'AIzaSyDxVQqaiKE1L6N9Etv9SUgKsEHfPr9Et40';
-
-export const CHAT_API_KEY = 'g9qa6ucyf3ff';
--- Toci.Driver.Common.Gatekeeper.Interfaces/ErrorHandling/IErrorFormatter.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Toci.Driver.Common.Gatekeeper.Interfaces.ErrorHandling
-{
-    public interface IErrorFormatter
-    {
-        void FormatError(IErrorEntity errorEntity, string applicationErrorMessage, string userErrorMessage);
-    }
-}
