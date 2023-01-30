f0a96c94bfa119daaa4ce7999cf03fe330d00b91
Bartomiej Zapart <bzapart@hotmail.com>
2023-01-30 10:28:23 +0100

    memcached poc.

---
+++ Intotech.Wheelo.Chat.Tests/CachingPoc.cs
@@ -0,0 +1,40 @@
+﻿using Enyim.Caching.Memcached;
+using MemcachedClient = Enyim.Caching.MemcachedClient;
+
+namespace Intotech.Wheelo.Chat.Tests;
+
+[TestClass]
+public class CachingPoc
+{
+    [Serializable]
+    public class doopa
+    {
+        public int id;
+    }
+
+    [TestMethod]
+    public void TestMemcached()
+    {
+        MemcachedCluster memCluster = new MemcachedCluster("localhost:11211");
+
+        memCluster.Start();
+
+        IMemcachedClient client = memCluster.GetClient();
+
+        bool res = client.SetAsync("Ghostrider", new doopa() { id = 8 }, Expiration.Never).Result;
+
+        var result = client.GetAsync("Ghostrider").Result;
+    }
+
+    [TestMethod]
+    public void GetFromMemcached()
+    {
+        MemcachedCluster memCluster = new MemcachedCluster("localhost:11212");
+
+        memCluster.Start();
+
+        IMemcachedClient client = memCluster.GetClient();
+
+        var result = client.GetAsync("Ghostrider").Result;
+    }
+}
