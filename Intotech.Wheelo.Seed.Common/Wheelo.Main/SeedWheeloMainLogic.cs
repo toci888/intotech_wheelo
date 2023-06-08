b389ff3011720709dd706137139d3d632b434a58
Bartłomiej Zapart <Bartlomiej.Zapart@grupawp.pl>
2023-06-08 09:29:33 +0200

    fix of seed hierarchy and build break.

--- Intotech.Wheelo.Seed.Common/Wheelo.Main/SeedWheeloMainLogic.cs
+++ Intotech.Wheelo.Seed.Common/Wheelo.Main/SeedWheeloMainLogic.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Intotech.Common.Bll.Seed;
 using Toci.Driver.Database.Persistence.Models;
 
 namespace Intotech.Wheelo.Seed.Common.Wheelo.Main
