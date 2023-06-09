6d1b7d3b1387e03572584200b5038a362483ebdf
Bartłomiej Zapart <Bartlomiej.Zapart@grupawp.pl>
2023-06-09 19:59:34 +0200

    xd

---
+++ Toci.Driver.Bll.Porsche.Interfaces/Intotech.Wheelo.Tests/Crap/MagicDtoFiller/AccountDtoLogic.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq.Expressions;
+using Intotech.Common.Bll.ChorDtoBll;
+using Intotech.Wheelo.Bll.Persistence;
+using Intotech.Wheelo.Tests.Crap.MagicDtoFiller.Dto;
+using Toci.Driver.Database.Persistence.Models;
+
+namespace Intotech.Wheelo.Tests.Crap.MagicDtoFiller;
+
+public class AccountDtoLogic : DtoLogicBase<Account, AccountLogic, AccountDto>
+{
+    public AccountDtoLogic(int accountId) : base(new AccountLogic(), m => m.Id == accountId)
+    {
+    }
+
+    protected override AccountDto FillEntity(AccountDto dto, Account field)
+    {
+        dto.Account = field;
+
+        return dto;
+    }
+}
---
+++ Toci.Driver.Bll.Porsche.Interfaces/Intotech.Wheelo.Tests/Crap/MagicDtoFiller/Dto/AccountDto.cs
@@ -0,0 +1,9 @@
+﻿using Intotech.Common.Bll.ChorDtoBll.Dto;
+using Toci.Driver.Database.Persistence.Models;
+
+namespace Intotech.Wheelo.Tests.Crap.MagicDtoFiller.Dto;
+
+public class AccountDto : DtoBase
+{
+    public Account Account { get; set; }
+}
