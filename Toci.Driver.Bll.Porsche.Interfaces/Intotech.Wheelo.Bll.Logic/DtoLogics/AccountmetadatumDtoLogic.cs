c3c55a7a8a9297a6ce9b0ef9dbdadf34e2205854
Bartłomiej Zapart <Bartlomiej.Zapart@grupawp.pl>
2023-06-13 09:11:31 +0200

    second example logic.

---
+++ Toci.Driver.Bll.Porsche.Interfaces/Intotech.Wheelo.Bll.Logic/DtoLogics/AccountmetadatumDtoLogic.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq.Expressions;
+using System.Reflection.Metadata.Ecma335;
+using Intotech.Common.Bll.ChorDtoBll;
+using Intotech.Common.Bll.ChorDtoBll.Dto;
+using Intotech.Wheelo.Bll.Models.Dtos;
+using Intotech.Wheelo.Bll.Models.ModelDtos.Intotech.Wheelo.Dtos;
+using Intotech.Wheelo.Bll.Persistence;
+using Toci.Driver.Database.Persistence.Models;
+
+namespace Intotech.Wheelo.Bll.Logic.DtoLogics;
+
+public class AccountmetadatumDtoLogic : DtoLogicBase<AccountmetadatumModelDto, Accountmetadatum, AccountMetadataLogic, AccountDto> 
+{
+    public AccountmetadatumDtoLogic(int accountId)
+        : base(new AccountMetadataLogic(), m => m.Idaccount == accountId,
+            (aDto, aMmDto) =>
+            {
+                aDto.Accountmetadatum = aMmDto;
+
+                return aDto;
+            })
+    {
+    }
+
+    protected override DtoBase<Accountmetadatum> GetDtoModelField(AccountDto dto)
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override AccountDto FillEntity(AccountDto dto, DtoBase<Accountmetadatum> field)
+    {
+        throw new NotImplementedException();
+    }
+}
