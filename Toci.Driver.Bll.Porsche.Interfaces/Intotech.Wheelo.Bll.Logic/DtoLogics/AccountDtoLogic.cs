a21ba318f47ab16c746f85d7048905b2646cc196
Bartłomiej Zapart <Bartlomiej.Zapart@grupawp.pl>
2023-06-13 08:48:01 +0200

    AccountDtoLogic - example of implementing logic for dto.

---
+++ Toci.Driver.Bll.Porsche.Interfaces/Intotech.Wheelo.Bll.Logic/DtoLogics/AccountDtoLogic.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq.Expressions;
+using Intotech.Common.Bll.ChorDtoBll;
+using Intotech.Common.Bll.ChorDtoBll.Dto;
+using Intotech.Wheelo.Bll.Models.Dtos;
+using Intotech.Wheelo.Bll.Models.ModelDtos.Intotech.Wheelo.Dtos;
+using Intotech.Wheelo.Bll.Persistence;
+using Intotech.Wheelo.Common.Interfaces.ModelMapperInterfaces;
+using Toci.Driver.Database.Persistence.Models;
+
+namespace Intotech.Wheelo.Bll.Logic.DtoLogics;
+
+public class AccountDtoLogic : DtoLogicBase<AccountModelDto, Account, AccountLogic, AccountDto>
+{
+    public AccountDtoLogic(int id) 
+        : base(new AccountLogic(), m => m.Id == id, 
+            (aDto, aModelDto) => { 
+                aDto.Account = aModelDto;
+                return aDto;
+            })
+    {
+    }
+
+    protected override DtoBase<Account> GetDtoModelField(AccountDto dto)
+    {
+        return dto.Account;
+    }
+
+    protected override AccountDto FillEntity(AccountDto dto, DtoBase<Account> field)
+    {
+        dto.Account = (AccountModelDto)field;
+
+        return dto;
+    }
+}
---
+++ Toci.Driver.Bll.Porsche.Interfaces/Intotech.Wheelo.Bll.Models/Dtos/AccountDto.cs
@@ -0,0 +1,12 @@
+﻿using Intotech.Wheelo.Bll.Models.ModelDtos.Intotech.Wheelo.Dtos;
+
+namespace Intotech.Wheelo.Bll.Models.Dtos;
+
+public class AccountDto
+{
+    public AccountModelDto Account { get; set; }
+
+    public AccountmetadatumModelDto Accountmetadatum { get; set; }
+
+    public AccountroleModelDto Accountrole { get; set; }
+}
