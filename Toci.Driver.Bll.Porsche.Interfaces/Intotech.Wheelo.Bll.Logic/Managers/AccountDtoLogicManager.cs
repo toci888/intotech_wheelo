b2a17fa0f1ff78f3714434112944eb4542eaab28
Bartłomiej Zapart <Bartlomiej.Zapart@grupawp.pl>
2023-06-13 22:41:50 +0200

    AccountDtoLogicManager - finish him ! xd

---
+++ Toci.Driver.Bll.Porsche.Interfaces/Intotech.Wheelo.Bll.Logic/Managers/AccountDtoLogicManager.cs
@@ -0,0 +1,14 @@
+﻿using Intotech.Common.Bll.ChorDtoBll;
+using Intotech.Wheelo.Bll.Logic.DtoLogics;
+using Intotech.Wheelo.Bll.Models.Dtos;
+
+namespace Intotech.Wheelo.Bll.Logic.Managers;
+
+public class AccountDtoLogicManager : DtoLogicManager<AccountDto>
+{
+    public AccountDtoLogicManager(int accountId)
+    {
+        AddDtoLogic(new AccountDtoLogic(accountId));
+        AddDtoLogic(new AccountmetadatumDtoLogic(accountId));
+    }
+}
