9bb4e70e5aebc37a24a3cb83b3813d8ad625d80f
toci888 <bzapart@gmail.com>
2023-08-02 17:30:41 +0200

    generated files.

---
+++ Toci.Driver.Bll.Porsche.Interfaces/Intotech.Wheelo.Bll.Models/DictionariesModelDto/CarsbrandModelDto.cs
@@ -0,0 +1,10 @@
+﻿using Intotech.Common.Bll.ChorDtoBll.Dto;
+using Toci.Driver.Database.Persistence.Models;
+
+namespace Intotech.Wheelo.Bll.Models.DictionariesModelDto;
+
+public class CarsbrandModelDto : DtoCollectionBase<Carsbrand, CarsbrandModelDto, List<Carsbrand>, List<CarsbrandModelDto>>
+{
+    public int Id { get; set; }
+    public string Brand { get; set; }
+}
