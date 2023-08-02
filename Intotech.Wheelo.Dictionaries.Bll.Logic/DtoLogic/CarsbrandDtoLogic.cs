dba1b05bdfbc29fe83412a2ccf4d68ec96cadbde
STANISLAV KHAVERKO <stas.xaverko@gmail.com>
2023-08-02 18:33:30 +0200

    generate Dictionaries DtoLogic. IDtoLogic, some fix

---
+++ Intotech.Wheelo.Dictionaries.Bll.Logic/DtoLogic/CarsbrandDtoLogic.cs
@@ -0,0 +1,36 @@
+﻿using Intotech.Common.Bll.ChorDtoBll;
+using Intotech.Wheelo.Bll.Models.DictionariesModelDto;
+using Intotech.Wheelo.Bll.Models.DictionariesDto;
+using Intotech.Wheelo.Dictionaries.Bll.Persistence.Interfaces;
+using Intotech.Wheelo.Dictionaries.Database.Persistence.Models;
+using Intotech.Wheelo.Dictionaries.Bll.Logic.Interfaces.IDtoLogic;
+using Intotech.Common.Bll.ChorDtoBll.Dto;
+
+namespace Intotech.Wheelo.Dictionaries.Bll.Logic.DtoLogic;
+
+public class CarsbrandDtoLogic : DtoLogicBase<CarsbrandModelDto, Carsbrand, ICarsbrandLogic, CarsbrandDto, List<Carsbrand>, List<CarsbrandModelDto>>, ICarsbrandDtoLogic
+{
+    public CarsbrandDtoLogic(ICarsbrandLogic carsbrandlogic) 
+        : base(carsbrandlogic, 
+            (aDto, aModelDto) => { 
+                aDto.Carsbrand = aModelDto;
+                return aDto;
+            })
+    {
+    }
+
+    protected override DtoBase<Carsbrand,CarsbrandModelDto> GetDtoModelField(CarsbrandDto dto)
+    {
+       return dto.Carsbrand;
+    }
+
+    protected override CarsbrandDto FillEntity(CarsbrandDto dto, CarsbrandModelDto  field)
+    {
+        dto.Carsbrand = field;
+
+        return dto;
+    }    protected override CarsbrandDto FillEntity(CarsbrandDto dto, List<CarsbrandModelDto> field)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Intotech.Wheelo.Dictionaries.Bll.Logic/DtoLogic/ColourDtoLogic.cs
@@ -0,0 +1,36 @@
+﻿using Intotech.Common.Bll.ChorDtoBll;
+using Intotech.Wheelo.Bll.Models.DictionariesModelDto;
+using Intotech.Wheelo.Bll.Models.DictionariesDto;
+using Intotech.Wheelo.Dictionaries.Bll.Persistence.Interfaces;
+using Intotech.Wheelo.Dictionaries.Database.Persistence.Models;
+using Intotech.Wheelo.Dictionaries.Bll.Logic.Interfaces.IDtoLogic;
+using Intotech.Common.Bll.ChorDtoBll.Dto;
+
+namespace Intotech.Wheelo.Dictionaries.Bll.Logic.DtoLogic;
+
+public class ColourDtoLogic : DtoLogicBase<ColourModelDto, Colour, IColourLogic, ColourDto, List<Colour>, List<ColourModelDto>>, IColourDtoLogic
+{
+    public ColourDtoLogic(IColourLogic colourlogic) 
+        : base(colourlogic, 
+            (aDto, aModelDto) => { 
+                aDto.Colour = aModelDto;
+                return aDto;
+            })
+    {
+    }
+
+    protected override DtoBase<Colour,ColourModelDto> GetDtoModelField(ColourDto dto)
+    {
+       return dto.Colour;
+    }
+
+    protected override ColourDto FillEntity(ColourDto dto, ColourModelDto  field)
+    {
+        dto.Colour = field;
+
+        return dto;
+    }    protected override ColourDto FillEntity(ColourDto dto, List<ColourModelDto> field)
+    {
+        throw new NotImplementedException();
+    }
+}
