ef7625b274daa57d8c61c556c6de2d896943f916
STANISLAV KHAVERKO <stas.xaverko@gmail.com>
2023-08-25 18:39:43 +0200

    account screen

--- ClientMobile/components/UnitButton.tsx
+++ ClientMobile/components/UnitButton.tsx
@@ -1,3 +1,5 @@
+
+import React from "react";
 import { TouchableOpacity, StyleSheet } from "react-native";
 import { MaterialIcons } from "@expo/vector-icons";
 import { Text } from "@ui-kitten/components";
--- ClientMobile/screens/AccountScreen.tsx
+++ ClientMobile/screens/AccountScreen.tsx
@@ -1,129 +1,122 @@
-import { ScrollView, View, StyleSheet, Linking, Dimensions, Image } from "react-native";
+import { ScrollView, View, StyleSheet, Linking, Dimensions, Image, TouchableOpacity, ImageSourcePropType, Switch } from "react-native";
+import React, { useState } from "react";
 import { useNavigation } from "@react-navigation/native";
 import { Text, Button } from "@ui-kitten/components";
-
 import { Screen } from "../components/Screen";
 import { SignUpAndSignInButtons } from "../components/SignUpAndSignInButtons";
 import { theme } from "../theme";
 import { ButtonList } from "../components/ButtonList";
 import { useUser } from "../hooks/useUser";
-import React from "react";
 import { i18n } from "../i18n/i18n";
 import useTheme from "../hooks/useTheme";
 import useColorScheme from "../hooks/useColorScheme";
 import { themes } from "../constants/constants";
+import { array } from "yup/lib/locale";
+import { Colors } from "react-native/Libraries/NewAppScreen";
+
+
+
+type AccountItem = {
+  type: "switch" | "button",
+  label: string,
+  onPress: () => void,
+  icon: ImageSourcePropType
+}
 
 export const AccountScreen = () => {
+  const [isEnabled, setIsEnabled] = useState(false);
   const { user, logout } = useUser();
   const { colors } = useTheme();
   const colorScheme = useColorScheme();
   const navigation = useNavigation();
 
-  const firstSignedOutButtons = [
+  const firstSignedOutButtons: AccountItem[] = [
     {
-      label: "Add a Property",
+      type: "button",
+      label: "Edytuj 1profil",
       onPress: () => navigation.navigate("AddProperty"),
+      icon: require('../assets/images/NoImage.jpeg')
     },
     {
-      label: "View My Properties",
+      type: "button",
+      label: "Zapisane trasy",
       onPress: () => navigation.navigate("MyProperties"),
-    },
-  ];
-
-  const supportButtons = [
-    {
-      label: "Help Center",
-      onPress: () => console.log("navigate to Help Center"), //navigation.navigate("AddProperty")
+      icon: require('../assets/images/NoImage.jpeg')
     },
     {
-      label: "Terms and Conditions",
-      onPress: () => console.log("navigate to Terms and Conditions"),
-    },
-  ];
-
-  const rentingButtons = [
-    {
-      label: "Favorite Properties",
-      onPress: () => navigation.navigate("Root", { screen: "Saved" }),
-    },
-    {
-      label: "Rental Applications",
-      onPress: () => console.log("navigate to Rental Applications"),
-    },
-    {
-      label: "My Residences",
+      type: "button",
+      label: "Ustawienia",
       onPress: () => navigation.navigate("MyProperties"),
+      icon: require('../assets/images/NoImage.jpeg')
     },
     {
-      label: "Rent Payments",
-      onPress: () => console.log("navigate to Rent Payments"),
-    },
-  ];
-
-  const accountButtons = [
-    {
-      label: "Account Settings",
-      onPress: () =>
-        navigation.navigate("Root", {
-          screen: "AccountRoot",
-          params: {
-            screen: "Settings",
-          },
-        }),
-    },
-    {
-      label: "Billing History",
-      onPress: () => console.log("navigate to Billing History"),
-    },
-    {
-      label: "Banks and Cards",
-      onPress: () => console.log("navigate to Banks and Cards"),
-    }
-  ];
-
-  const rentalManagementButtons = [
-    {
-      label: "Add a Property",
-      onPress: () => navigation.navigate("AddProperty"),
-    },
-    {
-      label: "Add Apartment to Property",
-      onPress: () => console.log("navigate to MyProperties"),
-    },
-    {
-      label: "View My Properties",
+      type: "button",
+      label: "Wiadomości",
       onPress: () => navigation.navigate("MyProperties"),
+      icon: require('../assets/images/NoImage.jpeg')
+    },
+    {
+      type: "button",
+      label: "Bezpieczeństwo i prywatność",
+      onPress: () => navigation.navigate("MyProperties"),
+      icon: require('../assets/images/NoImage.jpeg')
+    },
+    {
+      type: "switch",
+      label: "Powiadomienia",
+      onPress: () => navigation.navigate("MyProperties"),
+      icon: require('../assets/images/NoImage.jpeg')
     },
   ];
+
+  const AccManagementButtons = ({ item }: { item: AccountItem }) => {
+    return (
+      <TouchableOpacity style={styles.buttonContainer} onPress={item.onPress} key={item.label}>
+        <Image style={styles.buttonPhoto} source={item.icon} />
+        <Text style={[styles.buttonTextContainer,{color: colors.text}]}>{item.label}</Text>
+      </TouchableOpacity>
+    );
+  };
+
+  const AccManagementsSwitches = ({ item }: { item: AccountItem }) => {
+    return (
+      <TouchableOpacity style={[styles.buttonContainer, {flexDirection: "row"}]} onPress={item.onPress} key={item.label}>
+        <Text style={[styles.switchText,{color: colors.text}]}>{item.label}</Text>
+        <Switch style={styles.switch}
+          trackColor={{ false: '#767577', true: '#81b0ff' }}
+          thumbColor={isEnabled ? theme["color-primary-200"] : '#f4f3f4'}
+          ios_backgroundColor="#3e3e3e"
+          onValueChange={toggleSwitch}
+          value={isEnabled} />
+      </TouchableOpacity>
+    );
+  };
+
+  const toggleSwitch = () => {
+    setIsEnabled((previousState) => !previousState);
+  };
 
   return (
     <Screen>
-      {!user ? (
-        <View style={styles.defaultMarginHorizontal}>
-          {colorScheme === themes.light ?
-            <Image style={styles.logo} source={require('../assets/images/wheelo.png')} />
-            : <Image style={styles.logo} source={require('../assets/images/wheelo-darkTheme.png')} />}
-          <Text style={[styles.header, { color: colors.text }]} category={"h4"}>
-            {i18n.t('Welcome')}!
-          </Text>
-          <SignUpAndSignInButtons />
-        </View>
-      ) : (
-        <ScrollView style={styles.container}>
-          <Text style={[styles.userName, { color: colors.text }]} category={"h4"}>
-            {user.firstName ? `${i18n.t('Welcome')}, ${user.firstName}` : ""}
-          </Text>
-          <Text style={styles.email} category={"h6"}>
-            {user.email}
-          </Text>
-          <ButtonList data={rentingButtons} header={"Renting Made Easy"} />
-          <ButtonList data={accountButtons} header={"My Account"} />
-          <ButtonList data={rentalManagementButtons} header={"Rental Manager Tools"} />
-          <ButtonList data={firstSignedOutButtons} header="Properties" borderTop />
-          <ButtonList data={supportButtons} header="Support" marginTop borderTop />
-          <View style={[styles.specialMarginVertical, styles.defaultMarginHorizontal,]}>
-            <Button appearance={"ghost"} style={styles.button} onPress={logout}>{i18n.t('SignOut')}</Button>
+      {user && (
+        <ScrollView style={styles.mainContainer}>
+          <View style={[styles.container, {backgroundColor: theme["color-primary-500"] /*colors.background*/ }]}>          
+              <Image style={styles.accountPhoto} source={user.image ? { uri: user.image } : require('../assets/images/AccountNoImage.png')} />
+              <Text style={[styles.userName, {color: theme["color-primary-100"] /*colors.text*/}]}> {user.firstName ?? ""} </Text>           
           </View>
+            <View style={[styles.container,{backgroundColor: theme["color-primary-500"]/*colors.background*/}]}>
+              {
+                firstSignedOutButtons.map(item => (
+                  item.type === "button" ?
+                    <AccManagementButtons item={item} />
+                    : <AccManagementsSwitches item={item} />
+                ))
+              }
+            </View>
+            <View style={[styles.specialMarginVertical, styles.defaultMarginHorizontal,]}>
+              <Button appearance={"ghost"} style={styles.button} onPress={logout}>{i18n.t('SignOut')}</Button>
+            </View>
+          
         </ScrollView>
       )}
     </Screen>
@@ -131,21 +124,61 @@ export const AccountScreen = () => {
 };
 
 const styles = StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+  },
+  container: {
+    borderRadius: 20,
+    marginTop: 20,
+    marginBottom: 20,
+    marginLeft: 16,
+    marginRight: 16,
+    paddingTop: 16,
+    paddingBottom: 16,
+  },
+  infoContainer: {
+    flex: 1
+  },
+  buttonsContainer: {
+    flex: 1,
+  },
+  accountPhoto: {
+    width: 90,
+    height: 90,
+    alignSelf: "center",
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+  },
+  buttonPhoto: {
+    width: 30,
+    height: 24,
+    marginLeft: 20
+  },
+  buttonTextContainer: {
+    marginBottom: 40,  
+    marginLeft: 20,
+  },
+  switch: {
+    backgroundColor: 'blue',
+    marginLeft: 300
+  },
+  switchText: {
+    marginTop: 10,
+    marginLeft: 20
+  },
   lottie: {
     marginBottom: 50,
     height: 250,
     width: 250,
     alignSelf: "center",
   },
-  container: {
-    flex: 1,
-  },
   defaultMarginHorizontal: { marginHorizontal: 10 },
   userName: {
-    textAlign: "center",
-    fontWeight: "600",
-    marginBottom: 5,
-    textTransform: "capitalize",
+    marginTop: 13,
+    fontSize: 16,
+    alignSelf: "center",
+    textAlign: "center"
   },
   email: {
     textAlign: "center",
