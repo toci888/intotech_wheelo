d1b48df8edff651a86c9aff3e624207652860726
Karol <114170012+karol199393@users.noreply.github.com>
2023-08-26 20:55:31 +0200

    Upgrade Karol

--- ClientMobile/components/SignUpAndSignInButtons.tsx
+++ ClientMobile/components/SignUpAndSignInButtons.tsx
@@ -12,6 +12,10 @@ import useTheme from "../hooks/useTheme";
 import { i18n } from "../i18n/i18n";
 import { WheeloLogo } from "./logos/WheeloLogo";
 import { WheeloIcon } from "./logos/WheeloIcon";
+import { GoogleLogos } from "./logos/GoogleLogos";
+import { FacebookLogos } from "./logos/FacebookLogos";
+import { LinkedLogo } from "./logos/LinkedLogo";
+import { AppleLogo } from "./logos/AppleLogo";
 
 export const SignUpAndSignInButtons = () => {
   const navigation = useNavigation();
@@ -23,6 +27,7 @@ export const SignUpAndSignInButtons = () => {
       <WheeloIcon />
       <Text style={styles.sloganTitle}>{i18n.t("WheeloSloganTitle")}</Text>
       <Text style={styles.sloganSubtitle}>{i18n.t("WheeloSloganSubtitle")}</Text>
+      
 
       <Pressable onPress={() => navigation.navigate("SignIn")} style={styles.signInButton}>
         <Text>{i18n.t("SignIn")}</Text>
@@ -32,6 +37,14 @@ export const SignUpAndSignInButtons = () => {
         <Text>{i18n.t("SignUp")}</Text>
       </Pressable>
       <Text style={styles.bodyText}>{i18n.t("OrSignInWith")}</Text>
+      <View style={styles.socialButtonContainer}>
+        <GoogleLogos />
+        <FacebookLogos />
+        <LinkedLogo/>
+        <AppleLogo/>
+      </View>
+     
+     
     </View>
   );
 };
@@ -42,14 +55,39 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
   },
-  sloganTitle: {
-
+  socialButtonContainer:
+  {
+    flexDirection: 'row', // Komponenty zostaną ułożone w jednej linii
+   
   },
+  sloganTitle: {
+    textAlign: "center",
+    fontFamily: "Open Sans",
+    fontSize: 46,
+    fontStyle: "normal",
+    fontWeight: "700",
+    lineHeight: 120,
+  },
+  
   sloganSubtitle: {
+    textAlign: "center",
+    fontFamily: "Open Sans",
+    fontSize: 46,
+    fontStyle: "normal",
+    fontWeight: "700",
+    lineHeight: 120,
 
   },
   signInButton: {
-
+  
+      display: 'flex',
+      paddingVertical: 14,
+      paddingHorizontal: 40,
+      alignItems: 'center',
+      alignSelf: 'stretch',
+      backgroundColor: 'blue', // Na chwile
+      borderRadius: 99,
+    
   },
   signUpButton: {
 
