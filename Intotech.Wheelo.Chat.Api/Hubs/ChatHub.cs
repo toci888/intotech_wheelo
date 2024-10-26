444eaa60b44b3462cdb0e04a331abda8d39e2cd2
toci888 <bzapart@gmail.com>
2024-10-26 17:42:56 +0200

    chat gpt - master

--- Intotech.Wheelo.Chat.Api/Hubs/ChatHub.cs
+++ Intotech.Wheelo.Chat.Api/Hubs/ChatHub.cs
@@ -1,17 +1,12 @@
 ﻿using System.Collections.Concurrent;
 using Intotech.Common;
 using Intotech.Wheelo.Chat.Database.Persistence.Models;
-using Intotech.Wheelo.Chat.Jaguar;
 using Intotech.Wheelo.Chat.Jaguar.Interfaces;
 using Intotech.Wheelo.Chat.Models;
 using Microsoft.AspNetCore.SignalR;
-using System.Xml.Linq;
 using Microsoft.AspNetCore.Authorization;
-using Intotech.Wheelo.Chat.Api.Attributes;
-using Toci.Driver.Database.Persistence.Models;
-using Intotech.Wheelo.Bll.Models.Gaf;
-using AutoMapper.Execution;
-using Intotech.Wheelo.Common.Interfaces.CachingService;
+using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace Intotech.Wheelo.Chat.Api.Hubs
 {
@@ -20,80 +15,65 @@ namespace Intotech.Wheelo.Chat.Api.Hubs
         private const string ClientReceiveMessageCallback = "getMessage";
         private const string RoomEstablished = "RoomEstablished";
         private const string ClientAddUserCallback = "session";
-        private const string InviteToConversationCallback = "InviteToConversation";
-        private const string RoomIdPattern = "{0}_RoomIdText";
-
-        private static readonly ConcurrentDictionary<string, string> _connectedUsers =
-            new ConcurrentDictionary<string, string>();
+        private const string TypingCallback = "userTyping";
+        private const string StopTypingCallback = "stopTyping";
+        private static readonly ConcurrentDictionary<string, string> _connectedUsers = new ConcurrentDictionary<string, string>();
 
         protected IChatUserService ChatUserService;
         protected IRoomService RoomService;
-        protected ICachingService CachingService;
         protected IChatNotificationsService ChatNotificationsService;
+
+        public ChatHub(IChatUserService chatUser, IRoomService roomService, IChatNotificationsService chatNotificationsService)
+        {
+            ChatUserService = chatUser;
+            RoomService = roomService;
+            ChatNotificationsService = chatNotificationsService;
+        }
 
         public override async Task OnDisconnectedAsync(Exception exception)
         {
             _connectedUsers.TryRemove(Context.ConnectionId, out string userId);
-
             await base.OnDisconnectedAsync(exception);
         }
 
-        public ChatHub(IChatUserService chatUser, IRoomService roomService, ICachingService cachingService,
-            IChatNotificationsService chatNotificationsService)
-        {
-            ChatUserService = chatUser;
-            RoomService = roomService;
-            CachingService = cachingService;
-            ChatNotificationsService = chatNotificationsService;
-        }
-
-
         [Authorize(Roles = "User")]
-        public async Task ConnectUser(int idAccount) // accountId room for synchronizations
+        public async Task ConnectUser(int idAccount)
         {
             ChatUserDto data = ChatUserService.Connect(idAccount);
-
             if (data == null)
             {
                 await Clients.Caller.SendAsync("connect_error", "Invalid userId");
-
                 return;
             }
-
             data.SessionId = Context.UserIdentifier;
-
             _connectedUsers.TryAdd(Context.ConnectionId, Context.UserIdentifier);
-
             List<string> userRoomIds = RoomService.GetAllRooms(Context.UserIdentifier);
-
             foreach (string userRoomId in userRoomIds)
             {
-                await Groups.AddToGroupAsync(Context.ConnectionId, userRoomId.ToString());
+                await Groups.AddToGroupAsync(Context.ConnectionId, userRoomId);
             }
-
-            await this.Clients.User(Context.UserIdentifier).SendAsync(ClientAddUserCallback, new { data });
+            await Clients.User(Context.UserIdentifier).SendAsync(ClientAddUserCallback, new { data });
         }
 
         [Authorize(Roles = "User")]
-        public async Task<RoomsDto> CreateRoom(int idAccount, List<int> remainingAccountIds) // TODO KACPER DOC
+        public async Task<RoomsDto> CreateRoom(int idAccount, List<int> remainingAccountIds)
         {
-
             RoomsDto room = RoomService.CreateRoom(idAccount, remainingAccountIds);
-
             foreach (RoomMembersDto member in room.RoomMembers)
             {
-                Clients.User(member.Email).SendAsync(RoomEstablished, new { room });
+                await Clients.User(member.Email).SendAsync(RoomEstablished, new { room });
             }
-
             return room;
         }
 
         [Authorize(Roles = "User")]
-        public virtual async Task JoinRoom(string roomId) // TODO KACPER DOC
+        public async Task JoinRoom(string roomId)
         {
             RoomService.ApproveRoom(roomId, Context.UserIdentifier, true);
-
-            await Groups.AddToGroupAsync(Context.ConnectionId, roomId.ToString());
+            await Groups.AddToGroupAsync(Context.ConnectionId, roomId);
+            // Fetch previous messages when joining the room
+            var messages = await ChatUserService.GetMessagesForRoom(roomId);
+            await Clients.Caller.SendAsync("loadMessages", messages);
         }
 
         [Authorize(Roles = "User")]
@@ -101,25 +81,26 @@ namespace Intotech.Wheelo.Chat.Api.Hubs
         {
             chatMessage.SenderEmail = Context.UserIdentifier;
             chatMessage = ChatUserService.SendMessage(chatMessage);
-
             if (chatMessage != null)
             {
-                //await Groups.AddToGroupAsync(Context.ConnectionId, chatMessage.ID.ToString());
-                //await Clients.OthersInGroup(chatMessage.RoomId).SendAsync(ClientReceiveMessageCallback, new { chatMessage });
-                try
-                {
-                    await Clients.Group(chatMessage.RoomId).SendAsync(ClientReceiveMessageCallback, new { chatMessage });
-                    //await Clients.User(Context.UserIdentifier).SendAsync(ClientReceiveMessageCallback, new { chatMessage });
-                    ChatNotificationsService.SendChatNotifications(chatMessage.RoomId, chatMessage.SenderEmail, chatMessage, _connectedUsers);
-                }
-                catch (Exception ex)
-                {
-
-                }
-            }                                                            //roomid
-
+                await Clients.Group(chatMessage.RoomId).SendAsync(ClientReceiveMessageCallback, new { chatMessage });
+                ChatNotificationsService.SendChatNotifications(chatMessage.RoomId, chatMessage.SenderEmail, chatMessage, _connectedUsers);
+            }
             return chatMessage;
         }
 
+        [Authorize(Roles = "User")]
+        public async Task Typing(string roomId)
+        {
+            await Clients.OthersInGroup(roomId).SendAsync(TypingCallback, Context.UserIdentifier);
+        }
+
+        [Authorize(Roles = "User")]
+        public async Task StopTyping(string roomId)
+        {
+            await Clients.OthersInGroup(roomId).SendAsync(StopTypingCallback, Context.UserIdentifier);
+        }
+
+        // Additional methods for file uploads, message history, etc., can be added here
     }
 }
